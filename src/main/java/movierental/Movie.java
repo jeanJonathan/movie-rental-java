0fc6b93e60b772dbad9f76bd45e98b9a7b0907c5
Jean Jonathan <jeanjonathank2@gmail.com>
2024-03-25 19:26:00 +0100

    Suppression des attributs et methodes inutilisable dans la classe Movie

--- src/main/java/movierental/Movie.java
+++ src/main/java/movierental/Movie.java
@@ -3,17 +3,12 @@ package movierental;
 public class Movie {
     //Reference pour la classe Price
     Price _price;
-    public static final int CHILDRENS = 2;
-    public static final int NEW_RELEASE = 1;
-    public static final int REGULAR = 0;
-
     private String _title;
 
     public Movie(String title, Price price) {
         _title = title;
         _price = price;
     }
-
     public double getCharge(int daysRented) {
         return _price.getCharge(daysRented);
     }
