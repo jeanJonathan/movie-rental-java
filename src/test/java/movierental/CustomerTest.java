6a14a6b70f14e338e5aa5c358ef274ae3020c7c3
Jean Jonathan <jeanjonathank2@gmail.com>
2024-03-26 14:23:19 +0100

    Séparément du Montant dû pour Chaque Type de Film

--- src/test/java/movierental/CustomerTest.java
+++ src/test/java/movierental/CustomerTest.java
@@ -7,25 +7,46 @@ import org.junit.Test;
 public class CustomerTest {
 
     @Test
-    public void test_movie_charge() {
+    public void test_regular_price() {
         Customer customer = new Customer("Bob");
         customer.addRental(new Rental(new Movie("Jaws", new RegularPrice()), 2));
         customer.addRental(new Rental(new Movie("Golden Eye", new RegularPrice()), 3));
-        customer.addRental(new Rental(new Movie("Short New", new NewReleasePrice()), 1));
-        customer.addRental(new Rental(new Movie("Long New", new NewReleasePrice()), 2));
-        customer.addRental(new Rental(new Movie("Bambi", new ChildrensPrice()), 3));
-        customer.addRental(new Rental(new Movie("Toy Story", new ChildrensPrice()), 4));
 
         String expected = "" +
                 "Rental Record for Bob\n" +
                 "\tJaws\t2.0\n" +
                 "\tGolden Eye\t3.5\n" +
+                "Amount owed is 5.5\n" +
+                "You earned 2 frequent renter points";
+        assertEquals(expected, customer.statement());
+    }
+    @Test
+    public void test_new_release_price() {
+        Customer customer = new Customer("Bob");
+        customer.addRental(new Rental(new Movie("Short New", new NewReleasePrice()), 1));
+        customer.addRental(new Rental(new Movie("Long New", new NewReleasePrice()), 2));
+
+        String expected = "" +
+                "Rental Record for Bob\n" +
                 "\tShort New\t3.0\n" +
                 "\tLong New\t6.0\n" +
+                "Amount owed is 9.0\n" +
+                "You earned 3 frequent renter points";
+
+        assertEquals(expected, customer.statement());
+    }
+    @Test
+    public void test_childrens_price() {
+        Customer customer = new Customer("Bob");
+        customer.addRental(new Rental(new Movie("Bambi", new ChildrensPrice()), 3));
+        customer.addRental(new Rental(new Movie("Toy Story", new ChildrensPrice()), 4));
+
+        String expected = "" +
+                "Rental Record for Bob\n" +
                 "\tBambi\t1.5\n" +
                 "\tToy Story\t3.0\n" +
-                "Amount owed is 19.0\n" +
-                "You earned 7 frequent renter points";
+                "Amount owed is 4.5\n" +
+                "You earned 2 frequent renter points";
 
         assertEquals(expected, customer.statement());
     }
